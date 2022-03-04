5aa2cf235cbf0426d2387810f82fbad4b45c2a96
Vignesh291292 <44498588+Vignesh291292@users.noreply.github.com>
2022-03-04 18:28:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 function compute() {
     //Get the values and calculate 
     var principal = parseFloat(document.getElementById("principal").value);
+    var biggerThanZero = parseInt(principal) > 0;
+    
     var rate = parseFloat(document.getElementById("rate").value);
     var years = parseInt(document.getElementById("years").value);
     var interest = principal * years * rate / 100;
     var yearInTheFuture = new Date().getFullYear() + years;
     //Create the Interest text
+    if (!biggerThanZero) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+    }else{
+        
     document.getElementById("result").innerHTML = "Interest : If you deposit <mark>" + principal + "</mark>,<br/>" +
         "at an interest rate of <mark>" + rate + "</mark>,<br/>" +
         "You will receive an amount of <mark>" + interest + "</mark>,<br/> " +
         "in the year <mark>" + yearInTheFuture + "</mark>";
+    }
 
 }
 
